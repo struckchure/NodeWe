7a0194ce6e9f2acfc11578ec8604fc4bcef27ed1
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2020-10-30 07:25:54 +0100

    gmail credentials

--- NodeWe/settings.py
+++ NodeWe/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ameenmohammed2311@gmail.com'
-EMAIL_HOST_PASSWORD = 'aiypwzqp12#google'
+EMAIL_HOST_USER = 'ameenem2702@gmail.com'
+EMAIL_HOST_PASSWORD = 'aiypwzqp12#'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
